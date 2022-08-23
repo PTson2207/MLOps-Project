434ad49dd63b9d794edbd96a8a7431d992809f4e
PTson2207 <sonphukl@gmail.com>
2022-08-23 08:26:53 +0700

    update now

---
+++ tests/code/test_main.py
@@ -0,0 +1,91 @@
+import shutil
+from pathlib import Path
+
+import mlflow
+import pytest
+from typer.testing import CliRunner
+
+import sys
+sys.path.insert(1, "./configs")
+sys.path.insert(1, "./tagifai")
+import config
+import main
+from main import app
+
+runner = CliRunner()
+args_fp = Path(config.BASE_DIR, "tests", "code", "test_args.json")
+
+
+def delete_experiment(experiment_name):
+    client = mlflow.tracking.MlflowClient()
+    experiment_id = client.get_experiment_by_name(experiment_name).experiment_id
+    client.delete_experiment(experiment_id=experiment_id)
+
+
+def test_etl_data():
+    result = runner.invoke(app, ["etl-data"])
+    assert result.exit_code == 0
+
+
+def test_label_data():
+    result = runner.invoke(
+        app,
+        [
+            "label-data",
+            f"--args-fp={args_fp}",
+        ],
+    )
+    assert result.exit_code == 0
+
+
+@pytest.mark.training
+def test_train_model():
+    experiment_name = "test_experiment"
+    run_name = "test_run"
+    result = runner.invoke(
+        app,
+        [
+            "train-model",
+            f"--args-fp={args_fp}",
+            f"--experiment-name={experiment_name}",
+            f"--run-name={run_name}",
+            "--test-run",
+        ],
+    )
+    assert result.exit_code == 0
+
+    # Delete experiment
+    delete_experiment(experiment_name=experiment_name)
+    shutil.rmtree(Path(config.MODEL_REGISTRY, ".trash"))
+
+
+@pytest.mark.training
+def test_optimize():
+    study_name = "test_optimization"
+    num_trials = 1
+    result = runner.invoke(
+        app,
+        [
+            "optimize",
+            f"--args-fp={args_fp}",
+            f"--study-name={study_name}",
+            f"--num-trials={num_trials}",
+        ],
+    )
+    assert result.exit_code == 0
+
+    # Delete study
+    delete_experiment(experiment_name=study_name)
+    shutil.rmtree(Path(config.MODEL_REGISTRY, ".trash"))
+
+
+def test_load_artifacts():
+    run_id = open(Path(config.CONFIG_DIR, "run_id.txt")).read()
+    artifacts = main.load_artifacts(run_id=run_id)
+    assert len(artifacts)
+
+
+def test_predict_tag():
+    text = "Transfer learning with transformers for text classification."
+    result = runner.invoke(app, ["predict-tag", f"--text={text}"])
+    assert result.exit_code == 0
